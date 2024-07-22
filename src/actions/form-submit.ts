1e6dc05907841bb437acb0a5f340088d04531e2f
raishudesu <bacaltosbaryshnikov@gmail.com>
2024-07-22 13:51:43 +0800

    add resend email service

--- src/actions/form-submit.ts
+++ src/actions/form-submit.ts
@@ -1,10 +1,9 @@
 "use server";
 
 import { FormSchema } from "@/lib/zod";
+import { Resend } from "resend";
 
 export const submitForm = async (prevState: any, formData: FormData) => {
-  // await new Promise((resolve) => setTimeout(resolve, 3000));
-
   const validatedFields = FormSchema.safeParse({
     firstName: formData.get("firstName"),
     lastName: formData.get("lastName"),
@@ -18,7 +17,32 @@ export const submitForm = async (prevState: any, formData: FormData) => {
     };
   }
 
-  console.log(validatedFields);
+  const htmlStringTemplate = `<div>
+                                <h1>
+                                  Form Submission from ${validatedFields.data.firstName} ${validatedFields.data.lastName}
+                                </h1>
+                                <p>${validatedFields.data.message}</p>
+                              </div>`;
+
+  try {
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: "bacaltosbaryshnikov@gmail.com",
+      subject: "Portfolio Form Submission",
+      html: htmlStringTemplate,
+    });
+
+    if (error) {
+      return { message: error };
+    }
+  } catch (error: any) {
+    return {
+      message: error.message,
+    };
+  }
+
   return {
     message: "Success",
   };
